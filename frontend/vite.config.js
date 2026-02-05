1035b7d9b9570852af3e7bcc6c1ae75101e7240d
GuilhermeMeiraNazar <guilhermen610@gmail.com>
2026-02-05 20:52:49 -0300

    chore: configura scripts de deploy e base path do vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // O base deve ser exatamente o nome do seu repositório no GitHub
+  base: '/Melanski_Sport/', 
 })
